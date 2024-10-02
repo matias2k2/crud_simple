b93212c03ded9bfb05f2008d526c152ec7576c20
Matias G Adelina <98082081+matias2k2@users.noreply.github.com>
2024-10-02 14:07:19 +0100

    primeroTeste

---
+++ matiasG/src/main/java/CrudSimples/matiasG/MatiasGApplication.java
@@ -0,0 +1,13 @@
+package CrudSimples.matiasG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatiasGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatiasGApplication.class, args);
+	}
+
+}
